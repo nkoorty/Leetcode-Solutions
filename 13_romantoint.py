0442f8b2b5bb023bf50d5031a87fca247ff3c893
Artemiy Malyshau <artemiymalyshau@Artemiys-MacBook-Pro.local>
2022-08-15 19:07:00 +0200

    added 13

--- 13_romantoint.py
+++ 13_romantoint.py
@@ -1,12 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        convert = {'I':1, 'V':5, 'X':10,'L':50,'C':100,'D':500, 'M':1000}
-        total = 0
-        for i in range(len(s)):
-            if i + 1 < len(s) and convert[s[i]] < convert[s[i+1]]:
-                total -= convert[s[i]]
-            else:
-                total += convert[s[i]]
-        return total
-    
+        roman = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+        }
+        res = 0
         
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
